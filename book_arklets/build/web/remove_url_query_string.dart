b2aefadad4162126c65de7facff238073e138bf8
pappes <pappes@gmail.com>
2016-06-30 19:47:20 +1000

    upgrade dart version and add logging to query sting

--- book_arklets/build/web/remove_url_query_string.dart
+++ book_arklets/build/web/remove_url_query_string.dart
@@ -1,11 +1,29 @@
 import 'dart:html';
 import 'package:pappes_web_utility/pappes_web_utility.dart';
+import 'package:logging/logging.dart' as logging;
 
-void main() {  
+void main() {
+
+  querySelector('#output').text = 'Dart is running1.';
+
+  logging.hierarchicalLoggingEnabled = true;
+  log.level = logging.Level.WARNING;
+  log.level = logging.Level.FINEST;
+  log.onRecord.listen((logging.LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    MyHtml.logMessageIntoHTMLBodyComment('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
+
+  querySelector('#output').text = 'Dart is running1.';
+
   MyHtml.iterateHTMLDOM(
       document.body, 
       (element) => MyHtml.alterAttribute(
           element, 
           'href', 
-          (url) => MyHtml.setUriParameters(url)));  
+          (url) => MyHtml.setUriParameters(url)));
+
+
+  querySelector('#output').text = 'Dart is running2.';
 }
